fd41fbc0dc1d1af9abe3ec884405bab30c2db830
Marcelo Martinez <marcelomst1@gmail.com>
2020-01-15 14:31:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 360) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
